11277c5e86974965f88b521072a8f66279a14022
Evelin <bellidoevelin@gmail.com>
2022-10-12 16:13:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-
-const menu_toggle = document.querySelector('.menu-toggle');
+   const menu_toggle = document.querySelector('.menu-toggle');
 		const sidebar = document.querySelector('.sidebar');
 
 		menu_toggle.addEventListener('click', () => {
 			menu_toggle.classList.toggle('is-active');
 			sidebar.classList.toggle('is-active');
 		});
+
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0; // Para safari
+    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+    }
